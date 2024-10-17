9336992effc1ed69e6ea9e78bb2c02bc6c5af1fe
noothangimounika <noothangimounika@gmail.com>
2024-10-17 22:34:21 +0530

    learnig node s

--- index.js
+++ index.js
@@ -22,3 +22,72 @@ console.log("Hello Node js")
 // console.log(http);
 // console.log(dt);
 
+
+//modules
+
+// const os= require("os")
+// // console.log(os)
+// console.log(os.type())
+// console.log(os.version())
+// console.log(os.freemem())
+// console.log(os.cpus())
+
+// const path=require("path")
+// console.log(path.dirname("file path is " + __filename))
+
+//fs modules
+const fs=require("fs")
+
+fs.readFile("demo.txt" , 'utf8', (err,data)=>{
+    if(err){
+        console.log(err)
+    }
+    console.log(data)
+})
+
+
+fs.writeFile('fsWriteFile.html','utf8', (err)=>{
+    if(err){
+        console.log(err)
+    }
+    console.log("create file succussfully")
+
+})
+
+fs.rename('fsWriteFile.html','fsWriteFile.js',(err)=>{
+    if(err){
+        console.log(err)
+    }
+    console.log("rename successfully")
+})
+
+
+fs.unlink('fsWriteFile.js',(err)=>{
+    if(err){
+        console.log(err)
+    }
+    console.log("delete successfully")
+})
+
+
+
+// http server modules
+const http=require('http')
+
+const myServer= http.createServer((request,response)=>{
+    response.write("Welcome to Server")
+    response.end()
+})
+
+myServer.listen(5500)
+
+
+
+// Local Modules
+
+const {addNumber,subNumber,mutNumber,divNumber,moduloNumber}=require('./localmodules')
+console.log("Total adding number is " + addNumber(10,5))
+console.log("Total sub number is " +subNumber(10,5))
+console.log("Total multple number is " + mutNumber(10,5))
+console.log("Total div number is " +divNumber(10,5))
+console.log("Total mudulo number is " +moduloNumber(10,5))
---
+++ localmodules.js
@@ -0,0 +1,21 @@
+function addNumber(a,b){
+    return a+b
+}
+
+function subNumber(a,b){
+    return a-b
+}
+
+function mutNumber(a,b){
+    return a*b
+}
+
+function divNumber(a,b){
+    return a/b
+}
+
+function moduloNumber(a,b){
+    return a%b
+}
+
+module.exports={addNumber,subNumber,mutNumber,divNumber,moduloNumber}
